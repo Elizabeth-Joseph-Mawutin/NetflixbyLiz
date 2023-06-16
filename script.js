438d5c3644b978809137c8caa5098545dbafd1eb
Elizabeth Joseph <lizzy22.mj@gmail.com>
2023-06-16 23:29:28 +0100

    updated the site

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function checkInput() {
+	var inputField = document.getElementById("myInput");
+	var inputValue = inputField.value;
+  
+	if (inputValue === "") {
+	  alert("Input field is empty!");
+	} else {
+	  window.location.href = "https://www.netflix.com/signup/registration?locale=en-NG"; 
+	}
+}
+  
+
+function checkInput2() {
+	var inputField = document.getElementById("myInput2");
+	var inputValue = inputField.value;
+  
+	if (inputValue === "") {
+	  alert("Input field is empty!");
+	} else {
+	  window.location.href = "https://www.netflix.com/signup/registration?locale=en-NG"; 
+	}
+}
+
 function showText() {
 	var textElement = document.getElementById("textToShow");
 	if (textElement.style.display === "none") {
@@ -7,4 +30,47 @@ function showText() {
 	};
 };
 
-if (email == '') alert ("Email cannot be empty");
+function showText2() {
+	var textElement = document.getElementById("textToShow2");
+	if (textElement.style.display === "none") {
+		textElement.style.display = "block";
+	  } else {
+		textElement.style.display = "none";
+	};
+};
+
+function showText3() {
+	var textElement = document.getElementById("textToShow3");
+	if (textElement.style.display === "none") {
+		textElement.style.display = "block";
+	  } else {
+		textElement.style.display = "none";
+	};
+};
+
+function showText4() {
+	var textElement = document.getElementById("textToShow4");
+	if (textElement.style.display === "none") {
+		textElement.style.display = "block";
+	  } else {
+		textElement.style.display = "none";
+	};
+};
+
+function showText5() {
+	var textElement = document.getElementById("textToShow5");
+	if (textElement.style.display === "none") {
+		textElement.style.display = "block";
+	  } else {
+		textElement.style.display = "none";
+	};
+};
+
+function showText6() {
+	var textElement = document.getElementById("textToShow6");
+	if (textElement.style.display === "none") {
+		textElement.style.display = "block";
+	  } else {
+		textElement.style.display = "none";
+	};
+};
